34c20380f4288a2e4f3ae47ae8bde7f373669654
Soumyakanta Mohanty <soumyakantam022@gmail.com>
2021-07-15 11:13:04 +0530

    ReactJs Assignment Day 2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from 'react'
+import './style.css'
+const Apple = () => {
+    let [score,setScore] = useState(0)
+    return(
+        <div>
+            <h1>Hey This is my Counter apllication</h1>
+            <h3>The value of score is {score}  </h3>   
+            <button onClick={()=>{score < 25? setScore(score+1): setScore(score=25)}}>Increment</button>
+            <button onClick={()=>{score > 0? setScore(score-1): setScore(score=0)}}>Decrement</button>
+            <button onClick={()=>{setScore(score=0)}}>Reset</button>
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        </div>
+    )
 }
-
-export default App;
+export default Apple
+// function ab(){
+//     score = score + 1;
+// }
+ 
+ 
